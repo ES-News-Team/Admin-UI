8d122ddb2f2d28377ba2361d870399eff2085bd2
Daniel Jr <daniel.souza.lima@outlook.com>
2022-03-29 00:23:00 -0300

    renomeir a rota criar-noticias, e criei atualiza_deletar_noticias()

--- app/views.py
+++ app/views.py
@@ -25,16 +25,44 @@ def login():
         # if response.status_code == '200':
         #     # YEAH!!!
         #     pass 
-        return redirect('/crud-noticias')
+        return redirect('/criar-noticias')
 
     return render_template('login.html')
 
 
-# ======================= crud-noticias =======================
-@admin_ui_service.route('/crud-noticias', methods=('GET', 'POST'))
+# ======================= criar-noticias =======================
+@admin_ui_service.route('/criar-noticias', methods=('GET', 'POST'))
 def postar_noticia():
     if request.method == 'POST':
         logger.info({**request.form})
 
-    return render_template('post_news.html')
+    return render_template('create_news.html')
+
+
+# ======================= updel-noticias =======================
+@admin_ui_service.route('/updel-noticias', methods=('GET', 'POST'))
+def atualiza_deletar_noticias():
+    if request.method == 'POST':
+        logger.info({**request.form})
+
+    return render_template('update_delete_news.html', list_of_news = [
+        {
+            "id": "ec5c011a-6478-4def-99e1-ed58bee20b47",
+            "assunto": 'artes',
+            "title": "art TITLE",
+            "conteudo": "TEXTOTEXTOTEXTOTEXTO..."
+        },
+        {
+            "id": "ceb82e0a-d086-4111-b558-8ce5db910b51",
+            "assunto": 'pets',
+            "title": "pet TITLE",
+            "conteudo": "TEXTOTEXTOTEXTOTEXTO..."
+        },
+        {
+            "id": "54b5ff2b-1f41-4880-8b17-5e78c613f3ba",
+            "assunto": 'ciencia',
+            "title": "ciencia TITLE",
+            "conteudo": "TEXTOTEXTOTEXTOTEXTO..."
+        }
+    ])
 
